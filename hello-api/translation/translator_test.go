cba762e9242baf6c4f51812fbfbc78ac7aad873c
dmaxim <danemaxim@gmail.com>
2023-02-19 09:53:30 -0500

    Add unit tests

---
+++ hello-api/translation/translator_test.go
@@ -0,0 +1,59 @@
+package translation_test
+
+import (
+	"testing"
+
+	"github.com/dmaxim/hello-api/translation"
+)
+
+
+func TestTranslate(t *testing.T) {
+	tt := []struct {
+		Word string
+		Language string
+		Translation string
+	}{
+		{
+			Word: "hello",
+			Language: "english",
+			Translation: "hello",
+		},
+		{
+			Word: "hello",
+			Language: "german",
+			Translation: "hallo",
+		},
+		{
+			Word: "hello",
+			Language: "finnish",
+			Translation: "hei",
+		},
+		{
+			Word: "hello",
+			Language: "dutch",
+			Translation: "",
+		},
+		{
+			Word: "bye",
+			Language: "german",
+			Translation: "",
+		},
+		{
+			Word: "hello",
+			Language: "German",
+			Translation: "hallo",
+		},
+		{
+			Word: "Hello",
+			Language: "german",
+			Translation: "hallo",
+		},
+	}
+
+	for _, test := range tt {
+		res := translation.Translate(test.Word, test.Language)
+		if res != test.Translation {
+			t.Errorf(`expected %s but received %s`, test.Translation, res)
+		}
+	}
+}
